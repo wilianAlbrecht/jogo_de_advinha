6100a5b446c90b118ccb0854f06c5799d05b22b2
wilianAlbrecht <wilian0012@hotmail.com>
2023-02-06 01:47:08 -0400

    first commit

---
+++ jogo_de_advinha.py
@@ -0,0 +1,22 @@
+# make a mini game where the computer randomly chooses a number and the user tries to guess it, at the end write on the screen the number of times the user tried until he got the correct number
+
+# faça um mini jogo em que a maquina escolhe aleatoriamente um numero e o usuario tente advinhar, no fim escreva na tela a quantidade de vezes que o usuario tentou até acertar o numero correto
+
+from advinha_PTBR import *
+from advinha_EN import *
+
+
+print("1-português")
+print("2-English")
+
+lang = input("")
+
+if lang == "1":
+    advinha_PTBR()
+if lang == "2":
+    advinha_EN()
+else:
+    print("digite uma opção valida.")
+    print("type a valid option.")
+
+print("Developed by Wilian ALbrecht")
